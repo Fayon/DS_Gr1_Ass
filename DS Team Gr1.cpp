8f861411941e6480474d9ae2cb6d2caf53bd3df7
Fayon <fmccalla2012@gmail.com>
2018-12-17 18:13:00 -0500

    Update DS Team Gr1.cpp

--- DS Team Gr1.cpp
+++ DS Team Gr1.cpp
@@ -1,5 +1,12 @@
-// C++ program to find the shortest path between 
-// a given source cell to a destination cell. 
+/*  This assignment is given by our lecturer Miss Natalie Rose as group assignmnet of the course
+Data structures and Filemanagement 2.
+The program is designed to find theshortest path to a destination through a maze.
+The authors of this program are named in the readme file attached.
+
+We used C++ programming language to find the shortest path between 
+a given source cell to a destination cell. 
+*/
+
 #include <bits/stdc++.h> 
 using namespace std; 
 #define ROW 15 
@@ -19,28 +26,23 @@ struct queueNode
     int distance;  // cell's distance of from the source 
 }; 
   
-// check whether given cell (row, col) is a valid 
-// cell or not. 
+// check whether given cell (row, col) is a valid cell or not. 
 bool isValid(int row, int col) 
 { 
-    // return true if row number and column number 
-    // is in range 
+    // return true if row number and column number is in range 
     return (row >= 0) && (row < ROW) && 
            (col >= 0) && (col < COL); 
 } 
   
-// These arrays are used to get row and column 
-// numbers of 4 neighbours of a given cell 
+// These arrays are used to get row and column numbers of 4 neighbours of a given cell 
 int rowNum[] = {-1, 0, 0, 1}; 
 int colNum[] = {0, -1, 1, 0}; 
 int R1, E2, R3, R5, R6, R7, E4, R8;
   
-// function to find the shortest path between 
-// a given source cell to a destination cell. 
+// function to find the shortest path between a given source cell to a destination cell. 
 int BFS(int mat[][COL], Point src, Point destination) 
 { 
-    // check source and destination cell 
-    // of the matrix have value 1 
+    // check source and destination cell of the matrix have value 1 
     if (!mat[src.grid_x][src.grid_y] || !mat[destination.grid_x][destination.grid_y]) 
         return -1; 
   
@@ -63,13 +65,11 @@ int BFS(int mat[][COL], Point src, Point destination)
         queueNode curr = q.front(); 
         Point point = curr.point; 
   
-        // If we have reached the destination cell, 
-        // we are done 
+        // If we have reached the destination cell, we are done 
         if (point.grid_x == destination.grid_x && point.grid_y == destination.grid_y) 
             return curr.distance; 
   
-        // Otherwise dequeue the front cell in the queue 
-        // and enqueue its adjacent cells 
+        // Otherwise dequeue the front cell in the queue and enqueue its adjacent cells 
         q.pop(); 
   
         for (int i = 0; i < 4; i++) 
